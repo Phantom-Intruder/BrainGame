50339f9f0216e58c1a51c878bdfb0cd91e000e58
Mewantha <mewa.bandara@gmail.com>
2017-02-12 17:46:10 +0530

    Added onclick listeners to all buttons

--- app/src/main/java/coursework/com/braingame/MainActivity.java
+++ app/src/main/java/coursework/com/braingame/MainActivity.java
@@ -2,6 +2,7 @@ package coursework.com.braingame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,76 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Main screen buttons
+    public void newGameButtonClicked(View view) {
+
+    }
+    public void exitButtonClicked(View view) {
+    }
+
+    public void aboutButtonClicked(View view) {
+    }
+
+    public void continueButtonClicked(View view) {
+    }
+
+    //Level screen buttons
+    public void noviceButtonClicked(View view) {
+    }
+
+    public void easyButtonClicked(View view) {
+    }
+
+    public void mediumButtonClicked(View view) {
+    }
+
+    public void guruButtonClicked(View view) {
+    }
+
+    //Number pad buttons
+    public void onButtonZeroClicked(View view) {
+    }
+
+    public void onButtonOneClicked(View view) {
+    }
+
+    public void onButtonTwoClicked(View view) {
+    }
+
+    public void onButtonThreeClicked(View view) {
+    }
+
+    public void onButtonFourClicked(View view) {
+    }
+
+    public void onButtonFiveClicked(View view) {
+    }
+
+    public void onButtonSixClicked(View view) {
+    }
+
+    public void onButtonSevenClicked(View view) {
+    }
+
+    public void onButtonEightClicked(View view) {
+    }
+
+    public void onButtonNineClicked(View view) {
+    }
+
+    public void onButtonDelClicked(View view) {
+    }
+
+    public void onButtonHashClicked(View view) {
+    }
+
+
+
+
+
+    public void onButtonMinusClicked(View view) {
+    }
+
+
 }
